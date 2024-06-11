c11190404e5169c6b9ccf9c0c595f410767085d2
Perpy-del <perpetualmeninwa@gmail.com>
2024-06-11 19:04:11 +0100

    fix: :bug: add cors to enable cross origin

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const accessLogStream = require('../config/logger');
 require('dotenv').config();
 const connectToDb = require('../config/database')
@@ -15,6 +16,7 @@ app.use(logger('combined', { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/api', function(req, res) {
     res.send('Welcome to MyQuizPal!!! 😎');
